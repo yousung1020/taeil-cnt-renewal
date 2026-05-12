acce83a0365cc055b62a7846e243060be0aa77c3
minsikmon <110793639+minsik1014@users.noreply.github.com>
2026-05-13 01:23:25 +0900

    feat:business (#21)
    * feat: 사업실적 하단페이지 생성 및 작업
    
    * feat: 빌드에러 수정
    
    ---------
    
    Co-authored-by: minsik1014 <ms03zzang@gmail.com>
    Co-authored-by: yousung1020 <choiyousung1020@gmail.com>

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,7 +1,6 @@
 import { Section1 } from "./section1/Section1";
 import { Section2 } from "./section2/Section2";
 import { Section3 } from "./section3/Section3";
-import Footer from "../components/Footer";
 import "./home.css";
 
 function Home() {
